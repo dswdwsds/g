38c824ff58d871a6e5f6c66722c1faf9b7c22e8e
dswdwsds <bwdhal92@gmail.com>
2026-01-14 19:16:29 +0200

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -17,4 +17,4 @@ export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const provider = new GoogleAuthProvider();
 
-export { signInWithPopup, signOut, onAuthStateChanged, collection, addDoc, query, where, onSnapshot, orderBy, serverTimestamp, doc, updateDoc };
+export { signInWithPopup, signOut, onAuthStateChanged, collection, addDoc, query, where, onSnapshot, orderBy, serverTimestamp, doc, updateDoc, getDoc };
